f3dcc08387b37ad2fc41fd26cf5d7b7d437e8dc6
stonewallstan <stanleyarmstrong31@gmail.com>
2018-07-28 16:09:36 -0700

    feat(./app ./add-list): Add 'add list' button

--- movie-lists/src/app/app.module.ts
+++ movie-lists/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddListComponent } from './add-list/add-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddListComponent
   ],
   imports: [
     BrowserModule
---
+++ movie-lists/src/app/movielists.ts
@@ -0,0 +1,6 @@
+export class Movielists {
+    id: number;
+    name: String;
+    genre: String;
+    movie: String;
+}
